ecf4d28b66826bb12ea2387f2b7e500ecc5d82c5
Rena Rallis and Steve Spitz <steve@>
2017-06-08 13:05:48 -0700

    add checks for each hundred down to 400 (untested)

--- js/scripts.js
+++ js/scripts.js
@@ -26,6 +26,19 @@ $(document).ready(function() {
     if ((parseInt(usernumber) >= 800 && parseInt(usernumber) < 900) || (parseInt(usernumber) >= 1800 && parseInt(usernumber) < 1900) || (parseInt(usernumber) >= 2800 && parseInt(usernumber) < 2900) || (parseInt(usernumber) >= 3800 && parseInt(usernumber) < 3900)) {
       resultcomment = resultcomment + "DCCC"
     }
+    if ((parseInt(usernumber) >= 700 && parseInt(usernumber) < 800) || (parseInt(usernumber) >= 1700 && parseInt(usernumber) < 1800) || (parseInt(usernumber) >= 2700 && parseInt(usernumber) < 2800) || (parseInt(usernumber) >= 3700 && parseInt(usernumber) < 3800)) {
+      resultcomment = resultcomment + "DCC"
+    }
+    if ((parseInt(usernumber) >= 600 && parseInt(usernumber) < 700) || (parseInt(usernumber) >= 1600 && parseInt(usernumber) < 1700) || (parseInt(usernumber) >= 2600 && parseInt(usernumber) < 2700) || (parseInt(usernumber) >= 3600 && parseInt(usernumber) < 3700)) {
+      resultcomment = resultcomment + "DC"
+    }
+    if ((parseInt(usernumber) >= 500 && parseInt(usernumber) < 600) || (parseInt(usernumber) >= 1500 && parseInt(usernumber) < 1600) || (parseInt(usernumber) >= 2500 && parseInt(usernumber) < 2600) || (parseInt(usernumber) >= 3500 && parseInt(usernumber) < 3600)) {
+      resultcomment = resultcomment + "D"
+    }
+    if ((parseInt(usernumber) >= 4400 && parseInt(usernumber) < 500) || (parseInt(usernumber) >= 1400 && parseInt(usernumber) < 1500) || (parseInt(usernumber) >= 2400 && parseInt(usernumber) < 2500) || (parseInt(usernumber) >= 3400 && parseInt(usernumber) < 3500)) {
+      resultcomment = resultcomment + "CD"
+    }
+
     $("#result").text(resultcomment)
     $("#warning").text(warningcomment)
 
